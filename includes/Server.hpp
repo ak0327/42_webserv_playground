69ca2a415fbeeb8571b79429c3060c1c36987780
takira <ca.fie.0220@gmail.com>
2023-08-08 16:21:31 +0900

    update(server): refactor server and sep func, will add class 'HttpRequest' and 'HttpResponse'

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -15,6 +15,10 @@
 #include <sstream>
 #include <exception>
 
+#include "View.hpp"
+#include "HttpRequest.hpp"
+#include "HttpResponse.hpp"
+
 #define SERVER_ADDR		"127.0.0.1"
 #define SERVER_PORT		8080
 #define BUF_SIZE		1024
@@ -37,12 +41,6 @@
 
 #define SIZE			(5 * 1024)
 
-#define FINISH			"finish\n"
-
-#define HTTP_VERSION		"HTTP/1.1"
-#define STATUS_OK			"200 OK"
-#define STATUS_NOT_FOUND	"404 Not Found"
-
 class Server {
  public:
 	Server();
@@ -54,12 +52,7 @@ class Server {
 	int	listen_fd_;
 	int	connect_fd_;
 	struct sockaddr_in	a_addr_;
-
 	char request_message_[SIZE];
-	std::string response_message_;
-	std::string method_;
-	std::string target_;
-	std::string body_;
 
 	void create_socket();
 	void set_socket_addr();
@@ -67,29 +60,10 @@ class Server {
 	void listen_socket() const;
 	void accept_connect();
 
-	std::string get_response_date() const;
-	std::string get_content_type() const;
-	std::string get_extension(const std::string &path) const;
-
 	ssize_t	recv_request_from_client();
 
 	void response_http_to_client();
-	void parse_request_message();
-	std::string get_request_method();
-	std::string get_request_target();
 
-	int get_status();
-	int get_processing(const char *file_path);
-	std::string get_dynamic_body_now();
-	std::string get_dynamic_body_show_request();
-
-	ssize_t create_response_message(int status);
-	std::string create_status_line(int status);
-	std::string create_response_header();
-	std::string get_content_len_str();
-	std::string create_response_body(int status);
-
-	ssize_t get_file_size(const char *path);
-	void send_response_to_client(ssize_t response_size);
+	void send_response_to_client(HttpResponse response);
 	void disconnect_from_client() const;
 };
--- srcs/server_dir/Server.cpp
+++ srcs/server_dir/Server.cpp
@@ -59,202 +59,6 @@ void Server::accept_connect() {
 	}
 }
 
-std::string Server::get_request_method() {
-	char	*line, *tmp_method;
-	char	cpy_request[SIZE];
-
-	memcpy(cpy_request, request_message_, SIZE);
-	line = strtok(cpy_request, "\n");
-	tmp_method = strtok(line, " ");
-	if (!tmp_method) {
-		throw std::runtime_error("fail to get request method");
-	}
-	return tmp_method;
-}
-
-
-std::string Server::get_request_target() {
-	char	*tmp_target;
-	std::string target;
-
-	errno = 0;
-	tmp_target = strtok(NULL, " ");
-	if (!tmp_target) {
-		throw std::runtime_error("fail to get request target");
-	}
-	target = tmp_target;
-
-	// method start with '/'
-	if (target == "/now" || target == "/show_request") {
-		return target;
-	}
-	if (target == "/") {
-		target = "www/index.html";
-	} else {
-		target = "www" + target;
-	}
-	return target;
-}
-
-void Server::parse_request_message() {
-	method_ = get_request_method();
-	target_ = get_request_target();
-}
-
-std::string Server::get_dynamic_body_now() {
-	std::ostringstream ss;
-
-	ss << "<html>\n"
-	   << "<body>\n"
-	   << "<h1>Now: " << get_response_date() << "</h1>\n"
-	   << "</body>\n"
-	   << "</html>";
-
-	return ss.str();
-}
-
-std::string Server::get_dynamic_body_show_request() {
-	std::ostringstream ss;
-
-	ss << "<html>\n"
-	   << "<body>\n"
-	   << "<h1>Request Message: </h1>\n"
-	   << "<pre>" << request_message_ << "</pre>\n"
-	   << "</body>\n"
-	   << "</html>";
-
-	return ss.str();
-}
-
-int Server::get_processing(const char *file_path) {
-	FILE	*f;
-	size_t	file_size;
-	char	buf[SIZE];
-
-	if (strcmp(file_path, "/now") == 0) {
-		body_ = get_dynamic_body_now();
-		return 200;
-	} else if (strcmp(file_path, "/show_request") == 0) {
-		body_ = get_dynamic_body_show_request();
-		return 200;
-	}
-
-	file_size = get_file_size(file_path);
-	if (file_size == 0) {
-		return 404;
-	}
-	f = fopen(file_path, "r");
-	std::fread(&buf, 1, file_size, f);
-	fclose(f);
-	buf[file_size] = '\0';
-	body_ = std::string(buf, file_size);
-	return 200;
-}
-
-std::string Server::get_response_date() const {
-	char		date[1024];
-	time_t		gmt_time;
-	size_t		n;
-	std::string ret_date;
-
-	time(&gmt_time);
-	n = strftime(date, 1024, "%a, %d %b %Y %X %Z", gmtime(&gmt_time));
-	ret_date = std::string(date, n);
-	return ret_date;
-}
-
-std::string Server::get_extension(const std::string &path) const {
-	size_t	ext_pos;
-
-	ext_pos = path.find_last_of('.');
-	if (ext_pos == std::string::npos) {
-		return "";
-	}
-	return path.substr(ext_pos + 1);
-}
-
-std::string Server::get_content_type() const {
-	std::string	ext_str;
-
-	ext_str = get_extension(target_);
-//	std::cout << "ext_str:" << ext_str << std::endl;
-	if (ext_str == "html" || target_ == "/now" || target_ == "/show_request")
-		return "text/html";
-	if (ext_str == "css")
-		return "text/css";
-	if (ext_str == "jpg")
-		return "image/jpg";
-	if (ext_str == "png")
-		return "image/png";
-	return "application/octet-stream";
-}
-
-std::string Server::create_status_line(int status) {
-	if (status == 200)
-		return HTTP_VERSION " " STATUS_OK "\r\n";
-	return HTTP_VERSION " " STATUS_NOT_FOUND "\r\n";
-}
-
-std::string Server::get_content_len_str() {
-	std::ostringstream	ss;
-
-	ss << body_.length() << "\r\n";
-	return ss.str();
-}
-
-std::string Server::create_response_header() {
-	std::string			response_header;
-
-	response_header = "";
-	response_header += "Date: " + get_response_date() + "\r\n";
-	response_header += "Content-Lenght: " + get_content_len_str();
-	response_header += "Connection-Type: " + get_content_type() + "\r\n";
-	return response_header;
-}
-
-std::string Server::create_response_body(int status) {
-	if (status == 404) {
-		return "<html><body><h1>404 Not Found</h1></body></html>";
-	}
-	return body_;
-}
-
-ssize_t Server::create_response_message(int status) {
-	std::string	status_line, response_header, response_body;
-
-	if (status != 200 && status != 404) {
-		return -1;
-	}
-
-	status_line = create_status_line(status);
-	response_header = create_response_header();
-	response_body = create_response_body(status);
-	response_message_ = status_line + response_header + "\r\n" + response_body;
-	return static_cast<ssize_t>(response_message_.length());
-}
-
-ssize_t Server::get_file_size(const char *path) {
-	ssize_t	size, read_size;
-	char	read_buf[SIZE];
-	int		fd;
-
-	fd = open(path, O_RDONLY);
-	if (fd == -1) {
-		return 0;
-	}
-	size = 0;
-	read_size = 1;
-	while (read_size > 0) {
-		read_size = read(fd, read_buf, SIZE);
-		if (read_size == -1) {
-			size = 0;
-			break;
-		}
-		size += read_size;
-	}
-	close(fd);
-	return size;
-}
 
 ssize_t Server::recv_request_from_client() {
 	ssize_t request_size;
@@ -269,26 +73,20 @@ ssize_t Server::recv_request_from_client() {
 	return request_size;
 }
 
-void Server::send_response_to_client(ssize_t response_size) {
+void Server::send_response_to_client(HttpResponse response) {
+	char	*response_message = response.get_response_message();
+	size_t	message_len = response.get_response_size();
 	ssize_t send_size;
 
 	errno = 0;
-	send_size = send(connect_fd_, response_message_.c_str(), response_size, FLAG_NONE);
+	send_size = send(connect_fd_, response_message, message_len, FLAG_NONE);
 	if (send_size == SEND_ERROR) {
 		throw std::runtime_error(strerror(errno));
 	}
 }
 
-int Server::get_status() {
-	if (method_ != "GET") {
-		return 404;
-	}
-	return get_processing(target_.c_str());
-}
-
 void Server::response_http_to_client() {
-	ssize_t	request_size, response_size;
-	int 	status;
+	ssize_t	request_size;
 
 	request_size = recv_request_from_client();
 	if (request_size == 0) {
@@ -296,15 +94,11 @@ void Server::response_http_to_client() {
 		return;
 	}
 //	std::cout << "\ncheck request_message:\n" << YELLOW << request_message_ << END << std::endl;
-	parse_request_message();
 
-	status = get_status();
-	response_size = create_response_message(status);
-	if (response_size == -1) {
-		throw std::runtime_error("error : not support status");
-	}
-//	std::cout << "\ncheck response_message:\n" << GREEN << response_message_ << END << std::endl;
-	send_response_to_client(response_size);
+	HttpRequest request = HttpRequest(request_message_);
+	HttpResponse response = HttpResponse(request.get_path());
+
+	send_response_to_client(response);
 }
 
 void Server::disconnect_from_client() const {
