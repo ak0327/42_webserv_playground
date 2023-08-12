1cc230d61547cfd7dace506742cd4363a5e90c08
takira <ca.fie.0220@gmail.com>
2023-08-12 13:50:27 +0900

    update(HttpResponse): many update and add cookie

--- includes/HttpResponse.hpp
+++ includes/HttpResponse.hpp
@@ -18,24 +18,60 @@
 
 # define SIZE		(5 * 1024)
 
+/* method */
 # define GET_METHOD		"GET"
 # define POST_METHOD	"POST"
+# define DELETE_METHOD	"DELETE"
 
+/* statuc */
 # define STATUS_200	"200 OK"
 # define STATUS_302 "302 Found"
 # define STATUS_404 "404 Not Found"
 # define STATUS_405 "405 Method Not Allowed"
 
-# define NOW_ENDPOINT			"/now"
-# define SHOW_REQUEST_ENDPOINT	"/show_request"
-# define LOGIN_ENDPOINT			"/login"
-# define USERPAGE_ENDPOINT		"/userpage"
-# define PARAMETERS_ENDPOINT	"/parameters"
+/* endpoint */
+# define ENDPOINT_NOW			"/now"
+# define ENDPOINT_SHOW_REQUEST	"/show_request"
+# define ENDPOINT_LOGIN			"/login"
+# define ENDPOINT_USERPAGE		"/userpage"
+# define ENDPOINT_PARAMETERS	"/parameters"
 
-#define WWW_ROOT_DIR	"www"
-#define INDEX_HTML		"/index.html"
+/* extension */
+# define EXTENSION_HTML	"html"
+# define EXTENSION_HTM	"htm"
+# define EXTENSION_CSS	"css"
+# define EXTENSION_TXT	"txt"
+# define EXTENSION_JPG	"jpg"
+# define EXTENSION_PNG	"png"
+# define OCTET_STREAM	"octet-stream"
+
+/* Content-Type */
+# define CONTENT_TEXT	"text/"
+# define CONTENT_IMAGE	"image/"
+# define CONTENT_APP	"application/"
+
+# define CONTENT_TYPE_HTML			"text/html"
+# define CONTENT_TYPE_HTM			"text/htm"
+# define CONTENT_TYPE_CSS			"text/css"
+# define CONTENT_TYPE_TXT			"text/plain"
+# define CONTENT_TYPE_JPG			"image/jpg"
+# define CONTENT_TYPE_PNG			"image/png"
+# define CONTENT_TYPE_OCTET_STREAM	"application/octet-stream"
+
+/* static */
+# define WWW_ROOT_DIR	"www"
 # define ROOT_PATH		"/"
+# define INDEX_HTML		"/index.html"
+# define LOGIN_HTML		"/login.html"
 
+/* header */
+# define COOKIE_KEY		"Cookie"
+# define SET_COOKIE_KEY	"Set-Cookie"
+# define LOCATION_KEY	"Location"
+
+# define CRLF		"\r\n"
+# define COLON		":"
+# define OWS		" "
 
 class HttpResponse {
  public:
@@ -46,7 +82,6 @@ class HttpResponse {
 	size_t get_response_size();
 
 	static std::string get_response_date();
-
 	void show_response() const;
 
  private:
@@ -55,19 +90,23 @@ class HttpResponse {
 	std::string body_;
 	std::string response_message_;
 	size_t response_size_;
+	std::map<std::string, std::string> header_elems_;
 
 	std::map<int, std::string> STATUS_CODES_;
+	std::map<std::string, std::string> CONTENT_TYPES_;
 
-	std::map<int, std::string> init_status_line();
+	std::map<int, std::string> init_status_codes();
+	std::map<std::string, std::string> init_content_types();
+	bool is_supported_content_type(const std::string &extension) const;
 
 	void get_http_response(HttpRequest const &request);
 
-	std::string get_body_and_status(HttpRequest const &request);
+	void get_body_and_status(HttpRequest const &request);
 	std::string get_response_body(const char *file_path,
 								  std::string const &received_request);
 
-	size_t create_response_message();
-	std::string create_status_line();
+	size_t create_response_message(HttpRequest const &request);
+	std::string create_status_line(std::string const &http_version);
 
 	std::string create_response_header();
 	std::string get_content_len_str();
@@ -77,4 +116,20 @@ class HttpResponse {
 
 	std::string get_content_type(std::string const &path);
 	std::string get_extension(const std::string &path) const;
+
+	void get_endpoint_now(HttpRequest const &request);
+	void get_endpoint_show_request(HttpRequest const &request);
+	void get_endpoint_login(HttpRequest const &request);
+	void get_endpoint_userpage(HttpRequest const &request);
+	void get_endpoint_parameters(HttpRequest const &request);
+	void get_static_file_content(const std::string &path);
+
+	std::string get_static_path(std::string const &path) const;
+	void request_login(HttpRequest const &request);
+
+	void add_to_header_elems(std::string const &key, std::string const &value);
+
+	std::map<std::string, std::string> get_cookie_pairs(std::string &cookie_value);
+	bool is_key_contains_map(const std::map<std::string, std::string> map, std::string const &key);
+
 };
--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -1,15 +1,16 @@
 #include "HttpResponse.hpp"
 
 HttpResponse::HttpResponse(HttpRequest const &request) {
-	STATUS_CODES_ = init_status_line();
+	STATUS_CODES_ = init_status_codes();
+	CONTENT_TYPES_ = init_content_types();
 	get_http_response(request);
 }
 
 HttpResponse::~HttpResponse() {}
 
-// todo: add more status
-std::map<int, std::string> HttpResponse::init_status_line() {
+std::map<int, std::string> HttpResponse::init_status_codes() {
 	std::map<int, std::string> status_codes;
+
 	status_codes[200] = STATUS_200;
 	status_codes[302] = STATUS_302;
 	status_codes[404] = STATUS_404;
@@ -17,49 +18,260 @@ std::map<int, std::string> HttpResponse::init_status_line() {
 	return status_codes;
 }
 
-void HttpResponse::get_http_response(HttpRequest const &request) {
-	status_code_ = get_body_and_status(request);
-	content_type_ = get_content_type(request.get_path());
-	response_size_ = create_response_message();
+std::map<std::string, std::string> HttpResponse::init_content_types() {
+	std::map<std::string, std::string> content_types;
+
+	content_types[EXTENSION_HTML] = CONTENT_TYPE_HTML;
+	content_types[EXTENSION_HTM] = CONTENT_TYPE_HTM;
+	content_types[EXTENSION_CSS] = CONTENT_TYPE_CSS;
+	content_types[EXTENSION_TXT] = CONTENT_TYPE_TXT;
+	content_types[EXTENSION_JPG] = CONTENT_TYPE_JPG;
+	content_types[EXTENSION_PNG] = CONTENT_TYPE_PNG;
+	content_types[OCTET_STREAM] = CONTENT_TYPE_OCTET_STREAM;
+	return content_types;
 }
 
-std::string HttpResponse::get_body_and_status(HttpRequest const &request) {
-	std::string	method = request.get_method();
-	std::string path = request.get_path();
-	std::string received_request = request.get_received_request();
-	FILE	*f;
-	size_t	file_size;
-	char	buf[SIZE];
+// todo: add_to_header_elems called in various func...
+// todo: if something failure, status="500 Internal Error"
+void HttpResponse::get_http_response(HttpRequest const &request) {
 
-	if (method != GET_METHOD && method != POST_METHOD) {
-		return STATUS_CODES_[404];
-	}
+	add_to_header_elems("Date", get_response_date());
+	add_to_header_elems("Server", "Webserv");
+	get_body_and_status(request);
+	content_type_ = get_content_type(request.get_path());
+	add_to_header_elems("Content-Length", get_content_len_str());
+	add_to_header_elems("Connection-Type", content_type_);
+	add_to_header_elems("Connection", "Close");
+	response_size_ = create_response_message(request);
+}
 
-	// todo: sep for view -> if POST, if GET
-	if (path ==  NOW_ENDPOINT) {
+void HttpResponse::get_endpoint_now(HttpRequest const &request) {
+	const std::string method = request.get_method();
+
+	if (method == GET_METHOD) {
 		body_ = get_dynamic_body_now();
-		return STATUS_CODES_[200];
-	} else if (path == SHOW_REQUEST_ENDPOINT) {
-		// todo: received_request -> request_line, header, body
-		body_ = get_dynamic_body_show_request(received_request);
-		return STATUS_CODES_[200];
+		status_code_ = STATUS_CODES_[200];
+		return;
+	}
+	if (method == POST_METHOD) {
+		status_code_ = STATUS_CODES_[405];
+		return;
+	}
+	if (method == DELETE_METHOD) {
+		status_code_ = STATUS_CODES_[405];
+		return;
+	}
+	status_code_ = STATUS_CODES_[405];
+}
+
+void HttpResponse::get_endpoint_show_request(HttpRequest const &request) {
+	const std::string method = request.get_method();
+
+	if (method == GET_METHOD) {
+		body_ = get_dynamic_body_show_request(request);
+		status_code_ = STATUS_CODES_[200];
+		return;
+	}
+	if (method == POST_METHOD) {
+		body_ = get_dynamic_body_show_request(request);
+		status_code_ = STATUS_CODES_[200];
+		return;
+	}
+	if (method == DELETE_METHOD) {
+		status_code_ = STATUS_CODES_[405];
+		return;
+	}
+	status_code_ = STATUS_CODES_[405];
+}
+
+// todo: tmp
+std::string tmp_get_username(std::vector<char> body) {
+	std::string body_str(body.begin(), body.end());
+	std::string username;
+	std::string::size_type	pos;
+
+	pos = body_str.find('=');
+	if (pos != std::string::npos) {
+		username = body_str.substr(pos + 1);
+	}
+	return username;
+}
+
+void HttpResponse::get_endpoint_login(HttpRequest const &request) {
+	const std::string method = request.get_method();
+
+	if (method == GET_METHOD) {
+//		std::cout << "/login GET" << std::endl;
+		get_static_file_content(LOGIN_HTML);
+		return;
+	}
+	if (method == POST_METHOD) {
+//		std::cout << "/login POST" << std::endl;
+		std::string username = tmp_get_username(request.get_body());
+
+//		std::cout << "username:" << username << std::endl;
+
+		add_to_header_elems(LOCATION_KEY, ENDPOINT_USERPAGE);
+		add_to_header_elems(SET_COOKIE_KEY, username);  // tmp
+//		get_endpoint_userpage(request);
+		body_ = render_user_page(username);
+		return;
+	}
+	if (method == DELETE_METHOD) {
+		status_code_ = STATUS_CODES_[405];
+		return;
+	}
+	status_code_ = STATUS_CODES_[405];
+}
+
+// todo: tmp
+/*
+ *  expected: key1=value1; key2=value2; ...
+ *  sep cookie unit by '; ' and key-value by '='
+ */
+std::map<std::string, std::string> HttpResponse::get_cookie_pairs(std::string &cookies) {
+	std::map<std::string, std::string> cookie_pairs;
+	std::string::size_type start = 0;
+	std::string::size_type end;
+
+	while ((end = cookies.find("; ", start)) != std::string::npos) {
+		std::string cookie = cookies.substr(start, end - start);
+		std::string::size_type separator = cookie.find('=');
+
+		if (separator != std::string::npos) {
+			std::string key = cookie.substr(0, separator);
+			std::string value = cookie.substr(separator + 1);
+			cookie_pairs[key] = value;
+		}
+		start = end + 2; // Skip over the "; "
 	}
 
+	// Handle the last cookie
+	std::string cookie = cookies.substr(start);
+	std::string::size_type separator = cookie.find('=');
+
+	if (separator != std::string::npos) {
+		std::string key = cookie.substr(0, separator);
+		std::string value = cookie.substr(separator + 1);
+		cookie_pairs[key] = value;
+	}
+
+	return cookie_pairs;
+}
+
+bool HttpResponse::is_key_contains_map(const std::map<std::string, std::string> map, std::string const &key) {
+	return map.find(key) != map.end();
+}
+
+void HttpResponse::get_endpoint_userpage(HttpRequest const &request) {
+	const std::string method = request.get_method();
+	std::string cookies;
+	std::map<std::string, std::string> cookie_pairs;
+	std::string username;
+
+	if (method == GET_METHOD || method == POST_METHOD) {
+		std::cout << "/userpage GET" << std::endl;
+
+		if (request.is_not_contains_header_key(COOKIE_KEY)) {
+			get_static_file_content(ENDPOINT_LOGIN);  // redirect to /login
+			add_to_header_elems(LOCATION_KEY, ENDPOINT_LOGIN);
+			status_code_ = STATUS_CODES_[302];
+			return;
+		}
+
+		cookies = request.get_header_value(COOKIE_KEY);
+		cookie_pairs = get_cookie_pairs(cookies);
+
+		if (!is_key_contains_map(cookie_pairs, "username")) {
+			get_static_file_content(ENDPOINT_LOGIN);  // redirect to /login
+			add_to_header_elems(LOCATION_KEY, ENDPOINT_LOGIN);
+			status_code_ = STATUS_CODES_[302];
+		}
+
+		username = cookie_pairs.find("username")->second;
+		std::cout << " username:" << username << std::endl;
+
+		get_static_file_content(ENDPOINT_USERPAGE);  // todo: username?
+		body_ = render_user_page(username);
+		status_code_ = STATUS_CODES_[200];
+		return;
+	}
+	if (method == DELETE_METHOD) {
+		status_code_ = STATUS_CODES_[405];
+		return;
+	}
+	status_code_ = STATUS_CODES_[405];
+
+}
+
+void HttpResponse::get_endpoint_parameters(HttpRequest const &request) {
+	const std::string method = request.get_method();
+
+	if (method == GET_METHOD) {
+		status_code_ = STATUS_CODES_[405]; // todo:tmp
+		return;
+	}
+	if (method == POST_METHOD) {
+		status_code_ = STATUS_CODES_[405]; // todo:tmp
+		return;
+	}
+	if (method == DELETE_METHOD) {
+		status_code_ = STATUS_CODES_[405];
+		return;
+	}
+	status_code_ = STATUS_CODES_[405];
+}
+
+std::string HttpResponse::get_static_path(const std::string &path) const {
 	if (path == ROOT_PATH) {
-		path = std::string(WWW_ROOT_DIR) + std::string(INDEX_HTML);
-	} else {
-		path = std::string(WWW_ROOT_DIR) + path;
+		return std::string(WWW_ROOT_DIR) + std::string(INDEX_HTML);
 	}
-	file_size = get_file_size(path.c_str());
+	return std::string(WWW_ROOT_DIR) + path;
+}
+
+void HttpResponse::get_static_file_content(const std::string &path) {
+	std::string	static_path;
+	FILE		*f;
+	size_t		file_size;
+	char		buf[SIZE];
+
+	static_path = get_static_path(path);
+//	std::cout << "static_path:" << static_path << std::endl;
+	file_size = get_file_size(static_path.c_str());
 	if (file_size == 0) {
-		return STATUS_CODES_[404];
+		status_code_ =  STATUS_CODES_[404];
+		return ;
 	}
-	f = fopen(path.c_str(), "r");
-	std::fread(&buf, 1, file_size, f);
-	fclose(f);
+	f = fopen(static_path.c_str(), "r");  // todo: error
+	std::fread(&buf, 1, file_size, f);  // todo: error
+	fclose(f);  // todo: error
 	buf[file_size] = '\0';
 	body_ = std::string(buf, file_size);
-	return STATUS_CODES_[200];
+	status_code_ = STATUS_CODES_[200];
+}
+
+void HttpResponse::get_body_and_status(HttpRequest const &request) {
+	std::string path = request.get_path();
+
+	printf("1\n");
+	if (path == ENDPOINT_NOW) {
+		return get_endpoint_now(request);
+	}
+	if (path == ENDPOINT_SHOW_REQUEST) {
+		return get_endpoint_show_request(request);
+	}
+	if (path == ENDPOINT_LOGIN) {
+		printf("1-1\n");
+		return get_endpoint_login(request);
+	}
+	if (path == ENDPOINT_USERPAGE) {
+		printf("1-2\n");
+		return get_endpoint_userpage(request);
+	}
+	if (path == ENDPOINT_PARAMETERS) {
+		return get_endpoint_parameters(request);
+	}
+	return get_static_file_content(request.get_path());
 }
 
 std::string HttpResponse::get_response_date() {
@@ -84,50 +296,48 @@ std::string HttpResponse::get_extension(std::string const &path) const {
 	return path.substr(ext_pos + 1);
 }
 
-std::string HttpResponse::get_content_type(std::string const &path) {
-	std::string	ext_str;
-
-	ext_str = get_extension(path);
-// 	std::cout << "ext_str:" << ext_str << std::endl;
-	if (ext_str == "html" || path == "/now" || path == "/show_request")
-		return "text/html";
-	if (ext_str == "css")
-		return "text/css";
-	if (ext_str == "jpg")
-		return "image/jpg";
-	if (ext_str == "png")
-		return "image/png";
-	return "application/octet-stream";
+bool HttpResponse::is_supported_content_type(const std::string &extension) const {
+	return CONTENT_TYPES_.find(extension) != CONTENT_TYPES_.end();
 }
 
-std::string HttpResponse::create_status_line() {
+// todo: after get body ?
+std::string HttpResponse::get_content_type(std::string const &path) {
+	std::string	extension;
+
+	extension = get_extension(path);
+// 	std::cout << "extension:" << extension << std::endl;
+	if (is_supported_content_type(extension))
+		return CONTENT_TYPES_[extension];
+	return CONTENT_TYPES_[CONTENT_TYPE_HTML];
+}
+
+std::string HttpResponse::create_status_line(std::string const &http_version) {
 	std::ostringstream	ss;
 
-	//todo
-	ss << "HTTP_VERSION" << " " << status_code_ << "\r\n";
+	ss << http_version << " " << status_code_ << CRLF;
 	return ss.str();
 }
 
 std::string HttpResponse::get_content_len_str() {
 	std::ostringstream	ss;
 
-	ss << body_.length() << "\r\n";
+	ss << body_.length();
 	return ss.str();
 }
 
 std::string HttpResponse::create_response_header() {
+	std::map<std::string, std::string>::const_iterator it;
 	std::string response_header;
 
-	response_header = "";
-	response_header += "Date: " + get_response_date() + "\r\n";
-	response_header += "Content-Lenght: " + get_content_len_str();
-	response_header += "Connection-Type: " + content_type_ + "\r\n";
+	for (it = header_elems_.begin(); it != header_elems_.end(); it++) {
+		response_header += it->first + COLON + OWS + it->second + CRLF;
+	}
 	return response_header;
 }
 
 std::string HttpResponse::create_response_body() {
 	if (status_code_ == STATUS_CODES_[404]) {
-		return "<html><body><h1>404 Not Found</h1></body></html>";
+		return get_not_found_body();
 	}
 	return body_;
 }
@@ -140,14 +350,14 @@ char *HttpResponse::get_response_message() {
 	return const_cast<char *>(response_message_.c_str());
 }
 
-size_t HttpResponse::create_response_message() {
+size_t HttpResponse::create_response_message(HttpRequest const &request) {
 	std::string	status_line, response_header, response_body;
 
 	// todo: error handring, status != 200 && status != 404
-	status_line = create_status_line();
+	status_line = create_status_line(request.get_http_version());
 	response_header = create_response_header();
 	response_body = create_response_body();
-	response_message_ = status_line + response_header + "\r\n" + response_body;
+	response_message_ = status_line + response_header + CRLF + response_body;
 	return static_cast<ssize_t>(response_message_.length());
 }
 
@@ -174,8 +384,19 @@ ssize_t HttpResponse::get_file_size(const char *path) {
 	return size;
 }
 
+// todo: error, duplicate check, and so on
+void HttpResponse::add_to_header_elems(const std::string &key, const std::string &value) {
+	header_elems_[key] = value;
+}
+
 // todo: not display image
 void HttpResponse::show_response() const {
 	std::cout << "RESPONSE MESSAGE:\n"
 	<< YELLOW << response_message_ << END << std::endl;
+//	<< " Response line: \n"
+//	<< YELLOW "   " << method_ << ", " << path_ << ", " << http_version_ << END "\n"
+//	<< " Response header :\n"
+//	<< YELLOW "   " << END "\n"
+//	<< " Response body :\n"
+//	<< YELLOW "   " << END "\n"
 }
