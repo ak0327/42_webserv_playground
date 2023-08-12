45a49a00b2fcab3f692a30119247273648f52793
takira <ca.fie.0220@gmail.com>
2023-08-12 13:40:25 +0900

    add(View): add method

--- includes/View.hpp
+++ includes/View.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
-#include <iostream>
-#include <sstream>
-#include <string>
+# include <iostream>
+# include <sstream>
+# include <string>
 
-#include "HttpResponse.hpp"
+# include "HttpRequest.hpp"
+# include "HttpResponse.hpp"
 
 std::string get_dynamic_body_now();
-std::string get_dynamic_body_show_request(std::string const &request_message);
+std::string get_dynamic_body_show_request(HttpRequest const &request);
+std::string render_user_page(const std::string &username);
+std::string get_not_found_body();
--- srcs/View.cpp
+++ srcs/View.cpp
@@ -1,26 +1,76 @@
 #include "View.hpp"
 
+/*
+ * @param HttpRequest request
+ * @return HttpResponse response; status_code, body
+ */
+
+std::string get_not_found_body() {
+	return "<html><body><h1>404 Not Found</h1></body></html>";
+}
+
+// HttpRequest request -> updated ?
 std::string get_dynamic_body_now() {
 	std::ostringstream ss;
 
-	ss << "<html>\n"
-	   << "<body>\n"
-	   << "<h1>Now: " << HttpResponse::get_response_date() << "</h1>\n"
-	   << "</body>\n"
-	   << "</html>";
+	ss << "<!DOCTYPE html>\n"
+		<< "<html>\n"
+		<< "  <head>\n"
+		<< "    <meta charset=\"UTF-8\">\n"
+		<< "      <title>now</title>\n"
+		<< "  </head>\n"
+		<< "  <body>\n"
+		<< "    <h1>Now: " << HttpResponse::get_response_date() << "</h1>\n"
+		<< "  </body>\n"
+		<< "</html>";
 
 	return ss.str();
 }
 
-std::string get_dynamic_body_show_request(std::string const &request_message) {
+// todo: use show_request() ?
+std::string get_dynamic_body_show_request(HttpRequest const &request) {
+	const std::string received_request = request.get_received_request();
+
 	std::ostringstream ss;
 
-	ss << "<html>\n"
-	   << "<body>\n"
-	   << "<h1>Request Message: </h1>\n"
-	   << "<pre>" << request_message << "</pre>\n"
-	   << "</body>\n"
+	ss << "<!DOCTYPE html>\n"
+		<< "<html>\n"
+		<< "  <head>\n"
+		<< "    <meta charset=\"UTF-8\">\n"
+		<< "      <title>show request</title>\n"
+		<< "  </head>\n"
+		<< "  <body>\n"
+		<< "    <pre>" << received_request << "</pre>\n"
+//		<< "    <pre>" << request.get_request_str() << "</pre>\n"
+		<< "  </body>\n"
+		<< "</html>";
+
+	return ss.str();
+}
+
+std::string render_user_page(const std::string &username) {
+	std::ostringstream ss;
+
+	ss << "<!DOCTYPE html>\n"
+	   << "<html>\n"
+	   << "  <head>\n"
+	   << "    <meta charset=\"UTF-8\">\n"
+	   << "      <title>user page</title>\n"
+	   << "  </head>\n"
+	   << "  <body>\n"
+	   << "    <h1>welcome " << username << "!</h1>\n"
+	   << "  </body>\n"
 	   << "</html>";
 
 	return ss.str();
 }
+
+// todo: endpoint -> std::string request_body or func_ptr(HttpRequest request)
+//std::map<std::string, std::string> url_views = {
+//		{"/now", ""},
+//		{"/show_request", ""},
+//		{"/parameters", ""},
+//		{"/set_cookie", ""},
+//		{"/login", ""},
+//		{"/userpage", ""},
+//};
